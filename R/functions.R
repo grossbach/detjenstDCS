4ad0310f7db5f1ba74a7301397261628307ed4fc
Michael Grossbach <michael.grossbach@hmtm-hannover.de>
2021-06-25 16:00:29 +0200

    Adds get_day_numbers() and get_correct_fnames()

--- R/functions.R
+++ R/functions.R
@@ -10,3 +10,42 @@ get_patient_code <- function(path) {
                         patient_codes[-1])
   return(patient_codes)
 }
+get_day_numbers <- function(filenames) {
+  numbers <- sort(unique(as.numeric(gsub("d",
+                                         "",
+                                         x = stringr::str_extract(basename(filenames),
+                                                                  pattern = "d\\d{1,2}")))))
+}
+get_correct_fnames <- function(all_fnames, p_code, day, time_, stat) {
+  fnames <- NULL
+  base_names <- basename(all_fnames)
+  p_code <- paste0(p_code, "_")
+  # day <- paste0("d", day, "_")
+  time_ <- paste0(time_, "_")
+  for (p in p_code) {
+    for (d in day) {
+      for (t in time_) {
+        for (s in stat) {
+          # if (nchar(s) == 0) {
+          #   ss <- ""
+          # } else {
+          #   ss <- s
+          # }
+          if (d < 43) {
+            test_fname <- paste0(p, paste0("d", d, "_"),
+                                 t, s, ".xlsx")
+          } else {
+            test_fname <- paste0(p, paste0("d", d, "_"),
+                                 s, ".xlsx") # there is no pre or post at d43
+          }
+          if (any(grepl(test_fname, base_names, fixed = TRUE))) {
+            fnames <- c(fnames,
+                        test_fname)
+            break
+          }
+        }
+      }
+    }
+  }
+  return(fnames)
+}
